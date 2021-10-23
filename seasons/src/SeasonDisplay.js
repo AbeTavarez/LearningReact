b3bf22cf46f7aa509a8d59428e25c65d45e30f27
abeTavarez <abeincwireless@gmail.com>
2021-10-23 17:52:02 -0400

    seasondisplay component

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const getSeason = (lat, lon, month) => {
-  
-  if (month > 2 && month < 9){
-    // summer months
-   return lat > 0 ? 'summer' : 'winter'
-  } else {
-    // winter months
-    return lat > 0 ? 'winter' : 'summer'
+const seasonConfig = {
+  summer: {
+    text: "Let's go to the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "It's chilly outside!",
+    iconName: 'snowflake'
   }
 }
 
-const SeasonDisplay = ({lat, lon, errorMsg}) => {
+//* gets the season based on location and month
+const getSeason = (lat, lon, month) => {
+  if (month > 2 && month < 9) {
+    // summer months
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    // winter months
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = ({ lat, lon, errorMsg }) => {
   const season = getSeason(lat, lon, new Date().getMonth());
 
-  return <div>Season: {season}</div>;
+  // season === 'summer' || 'winter' 
+  // seasonConfig[season] // {text, iconName}
+  const {text, iconName} = seasonConfig[season];
+  
+
+  return (
+    <div>
+      <i className={`${iconName} icon`}></i>
+      <h1>Season: {text}</h1>
+      <i className={`${iconName} icon`}></i>
+    </div>
+  )
 };
 
 export default SeasonDisplay;
