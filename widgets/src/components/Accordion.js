295506f23fe147b7fd2dff15d40fd372b606b8e3
abeTavarez <abeincwireless@gmail.com>
2021-11-01 23:25:24 -0400

    widget bp

---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from "react";
+
+const Accordion = ({items}) => {
+    const renderItems = items.map( item => (
+        <Fragment key={item.title}>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>{item.content}</p>
+            </div>
+        </Fragment>
+    ));
+
+    return <div className="ui styled accordion">{renderItems}</div>
+}
+
+export default Accordion;
