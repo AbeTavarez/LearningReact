c66a21f233e2175c46382fec4587d17d28a06bb8
abeTavarez <abeincwireless@gmail.com>
2021-11-03 11:33:42 -0400

    serach comp

---
+++ widgets/src/components/Search.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input type="text" className="input" value={term} onChange={setTerm}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
