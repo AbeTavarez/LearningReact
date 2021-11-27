ee8c1120e6c9ea3e562a4548c444bc069b561750
abeTavarez <abeincwireless@gmail.com>
2021-11-26 23:01:18 -0500

    go btn

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("programming");
-  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("programming"); // term to search
+  const [results, setResults] = useState([]); // results from search request
   console.log(results);
 
   //* fetch data
@@ -29,10 +29,17 @@ const Search = () => {
   }, [term]);
 
 
-  //* Create List of data
+  //* ========= Create List of data
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            >Go</a>
+            
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
