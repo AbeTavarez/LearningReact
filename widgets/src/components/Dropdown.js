c7eae0b47175c586832d12080e3aebb0c95c1924
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-04 14:54:19 -0500

    dropdown selected

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Dropdown = ({options, selected, onSelectedChange}) => {
-    const renderedOptions = options.map( option => {
-        return(
-            <div key={option.value} className='item'>
-                {option.label}
-            </div>
-        )
-    })
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const renderedOptions = options.map((option) => {
     return (
-       <div className='ui form'>
-           <div className='field'>
-                <label className='label'>Select a color</label>
-                <div className='ui selection dropdowm visible active'>
-                    <i className='dropdowm icon'></i>
-                    <div className='text'>{selected.label}</div>
-                    <div className='menu visible transition'>
-                        {renderedOptions}
-                    </div>
-                </div>
-           </div>
-       </div>
-    )
-}
+      <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div className="ui selection dropdowm visible active">
+          <i className="dropdowm icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
