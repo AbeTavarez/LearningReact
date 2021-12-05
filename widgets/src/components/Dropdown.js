852a03c94f2454318cef2f906e426625d3192952
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-04 21:48:15 -0500

    dropdown event listeners

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  // Data
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    };
     return (
-      <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
         {option.label}
       </div>
     );
   });
+
+
   return (
     <div className="ui form">
       <div className="field">
         <label className="label">Select a color</label>
-        <div className="ui selection dropdowm visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdowm ${open ? "visible active" : ""}`}
+        >
           <i className="dropdowm icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
