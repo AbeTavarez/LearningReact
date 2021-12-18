6de1ba9b813f55dc2db1eafe4b0e9ff95bb0a0d2
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-17 23:35:54 -0500

    react-Redux setup

--- blogapp/src/index.js
+++ blogapp/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+// Redux
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from './reducers';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
