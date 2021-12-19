194c30d076aa61bcec8887be2a30aff225a07eac
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-18 20:35:29 -0500

    Redux thunk middleware ready

--- blogapp/src/index.js
+++ blogapp/src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 
 // Redux
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
