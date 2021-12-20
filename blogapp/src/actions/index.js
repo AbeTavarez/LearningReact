b0d286fffc54db70b5c0b6b7c6a237f787ce197e
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-19 22:54:54 -0500

    update res -> res.data

--- blogapp/src/actions/index.js
+++ blogapp/src/actions/index.js
@@ -2,5 +2,5 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
         const res = await jsonPlaceholder.get('/posts');
-        dispatch({type: 'FETCH_POSTS', payload: res});
+        dispatch({type: 'FETCH_POSTS', payload: res.data});
 };
