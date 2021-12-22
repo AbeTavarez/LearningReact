82137aaef13c468559b77876bedc8d8332f3b94e
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-21 22:53:15 -0500

    redux dev tools installed

--- blogapp/src/index.js
+++ blogapp/src/index.js
@@ -6,11 +6,20 @@ import reportWebVitals from "./reportWebVitals";
 
 // Redux
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware, } from "redux";
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducers, applyMiddleware(thunk))
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+));
+
+
+// const store = createStore(reducers, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
