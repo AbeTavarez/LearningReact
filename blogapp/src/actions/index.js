0f4f8eb0bf395a4d1f69550cec77481c386197a3
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-21 21:27:01 -0500

    fetch users action creator function

--- blogapp/src/actions/index.js
+++ blogapp/src/actions/index.js
@@ -4,3 +4,8 @@ export const fetchPosts = () => async dispatch => {
         const res = await jsonPlaceholder.get('/posts');
         dispatch({type: 'FETCH_POSTS', payload: res.data});
 };
+
+export const fetchUser = () => {
+    const res = await jsonPlaceholder.get('/users');
+    dispatch({type: 'FETCH_USERS', payload: res.data});
+}
