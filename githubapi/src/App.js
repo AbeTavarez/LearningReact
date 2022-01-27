b2bf21f867c69a6fdfa0036f8f5d25225ccf13f3
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-26 23:07:24 -0500

    github api project

---
+++ githubapi/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>hey</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ githubapi/src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fab fa-github'
+    }
+    render() {
+        return (
+            <nav className='navbar bg-primary'>
+                
+                <h1>
+                <i className={this.props.icon}/>
+                 {' '}   {this.props.title}</h1>
+            </nav>
+        )
+    }
+}
+
+
+export default Navbar;
