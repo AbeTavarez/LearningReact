942830391a5f978a68eb91bdacbd68cca687328c
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-27 23:07:39 -0500

    fetching users

--- githubapi/src/App.js
+++ githubapi/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/layout/Navbar';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <h1>hey</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data });
+    this.setState({ loading: false });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
