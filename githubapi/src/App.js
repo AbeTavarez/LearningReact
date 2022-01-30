edb209bb1ee7e5acb283a44df75d7b1b5c5f2acc
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-29 23:47:03 -0500

    userItem component

--- githubapi/src/App.js
+++ githubapi/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -11,6 +12,7 @@ import axios from "axios";
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -36,6 +38,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Search single user
+  getUser = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({user: res.data, loading: false});
+  }
+
   // Clear users
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -46,7 +57,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -71,6 +82,9 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About}/>
+              <Route exact path='/user/:login' render={props => (
+                <User { ...props} getUser={this.getUser} user={user} loading={loading}/>
+              )}/>
             </Switch>
           </div>
         </div>
--- githubapi/src/components/users/UserItem.js
+++ githubapi/src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 const Useritem = (props) => {
   const { login, avatar_url, html_url } = props.user;
@@ -15,9 +16,9 @@ const Useritem = (props) => {
       <h3>{login}</h3>
 
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           more
-        </a>
+        </Link>
       </div>
     </div>
   );
@@ -25,7 +26,7 @@ const Useritem = (props) => {
 
 Useritem.propTypes = {
     user: PropTypes.object.isRequired,
-    
+
 }
 
 export default Useritem;
