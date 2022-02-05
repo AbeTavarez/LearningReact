63e743f4593a6de12e94030a34c25e6656bafdc3
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-04 19:53:26 -0500

    show answer btn

---
+++ ps/React/W1/D4/jeopardy/App.js
@@ -0,0 +1,56 @@
+class App extends React.Component {
+  state = {
+    question: [],
+  };
+
+  handleGetQuestion = async () => {
+    const res = await fetch("http://jservice.io/api/random");
+    const data = await res.json();
+    // console.log(data);
+    this.setState({ question: [data[0]] });
+    console.log(this.state.question);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome To React Jeopardy!</h1>
+
+        <button onClick={this.handleGetQuestion}>Get Question</button>
+
+        <Question question={this.state.question} />
+      </div>
+    );
+  }
+}
+
+class Question extends React.Component {
+    state = {
+        showAnswer: false
+    }
+
+    handleShowAnswer = () => {
+       this.setState({showAnswer: !this.state.showAnswer})
+    }
+  render() {
+    return (
+      <div>
+        {this.props.question.length
+          ? this.props.question.map((q) => (
+              <div>
+                <h3>Question: {q.question}</h3>
+                <h3>Category: {q.category.title}</h3>
+                <h3>Points: {q.value}</h3>
+                <div>
+                    <button onClick={this.handleShowAnswer}>Show Answer</button>
+                    {this.state.showAnswer ? <h3>Answer: {q.answer}</h3> : ''}
+                </div>
+              </div>
+            ))
+          : ""}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
