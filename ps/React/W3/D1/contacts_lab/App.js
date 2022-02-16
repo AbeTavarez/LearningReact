0695e6061d07cb20949a80fb5ac11a7971844d72
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-15 20:39:40 -0500

    giphy app

--- ps/React/W3/D1/contacts_lab/App.js
+++ ps/React/W3/D1/contacts_lab/App.js
@@ -1,8 +1,7 @@
 console.log(users);
-
+// Use functional components
 // Create App, Header, Footer, ContactList, ContactItem.
 // No need for state, just map over the users and show the data.
-//only Use functional components
 // ContactList will render multiple ContactItem components
 // style your app!!
 
